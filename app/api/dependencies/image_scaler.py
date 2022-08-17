ec0802291fffad5f4b9efe8f114d605c56a0ebe1
Jetstream <bigalex934@gmail.com>
2022-08-17 23:42:43 +0300

    Add type specifications in functions prototypes

--- app/api/dependencies/image_scaler.py
+++ app/api/dependencies/image_scaler.py
@@ -4,7 +4,7 @@ import base64
 from PIL import Image
 
 
-def encode_image(raw_image):
+def encode_image(raw_image: bytes):
     return base64.b64encode(raw_image)
 
 
@@ -12,7 +12,8 @@ def decode_image(encoded_image: str):
     return base64.b64decode(encoded_image)
 
 
-def scale_image(image_data, target_width: int, target_height: int):
+def scale_image(image_data: bytes, target_width: int, target_height: int):
     image_data = Image.open(io.BytesIO(image_data))
-    resized_image = image_data.resize((target_width, target_height))
+    target_size = (target_width, target_height)
+    resized_image = image_data.resize(target_size)
     return resized_image
